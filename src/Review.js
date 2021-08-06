f1796c28da6692e6db73898cd9c4111ea1fbe089
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-08-06 14:44:53 +0530

    btnHandeler

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [counter, setCounter] = useState(0);
+  const { id, name, job, image, text } = people;
+  const btnHandeler = () => {
+    setCounter((preid) => preid + 1);
+  };
+  return (
+    <div>
+      <h2>{people[counter].name}</h2>
+      <button onClick={btnHandeler}>Forward</button>
+    </div>
+  );
 };
 
 export default Review;
