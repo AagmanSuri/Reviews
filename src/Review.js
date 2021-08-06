72c397e9e9920517f1b6939ab13ba3c5528b3680
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-08-06 14:53:35 +0530

    card

--- src/Review.js
+++ src/Review.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import people from "./data";
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+//import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
   const [counter, setCounter] = useState(0);
-  const { id, name, job, image, text } = people;
   const btnHandeler = () => {
     setCounter((preid) => preid + 1);
   };
+  const backwardbtnHandeler = () => {
+    setCounter((preid) => preid - 1);
+  };
   return (
     <div>
-      <h2>{people[counter].name}</h2>
+      <img
+        style={{ height: "5rem" }}
+        src={people[counter].image}
+        alt="yes"
+      ></img>
+      <h3>Name: {people[counter].name}</h3>
+      <h4>Job : {people[counter].job}</h4>
+      <p>{people[counter].text}</p>
+
       <button onClick={btnHandeler}>Forward</button>
+      <button onClick={backwardbtnHandeler}>Backward</button>
     </div>
   );
 };
